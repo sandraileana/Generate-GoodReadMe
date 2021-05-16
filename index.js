fc7febc084fef53e745611aa1639af5b6cd5e02f
Sandra Ileana <sandraileana@gmail.com>
2021-05-15 21:24:31 -0500

    Homework first try

---
+++ index.js
@@ -0,0 +1,98 @@
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
+const markdown = require('./utilities/markdown');
+
+const questions = [
+    {
+        type: "input",
+        message: "Enter your GitHub username: ",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter your project title: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a brief project description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter the installation process: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter the usage of the project: ",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter the licenses used for the project: ",
+        name: "licenses"
+    },
+    {
+        type: "input",
+        message: "Enter the contribuiting information: ",
+        name: "contribute"
+    },
+    {
+        type: "input",
+        message: "Enter any tests you are running for your project: ",
+        name: "tests"
+    }
+];
+
+function init(){
+    return inquirer.prompt(questions);
+};
+
+function writeToFile(fileName, data){
+    fs.writeFile(`./${fileName}`, data, err => {
+        if (err) {
+            throw err
+        };
+        console.log("README written successfully");
+    });
+};
+
+init()
+
+.then(answers => markdown(answers))
+.then(ReadMeGenerated => writeToFile('ReadMeGenerated.md', ReadMeGenerated))
+.catch(err => {
+    console.log(err);
+});
+
+// {
+//         const UserName = answers.username;
+//         const ApiData = api.getUser(UserName);
+//         ApiData.then(res => {
+//             const data = 
+//             {
+//                 name: res.name,
+//                 username: answers.username,
+//                 url: res.html_url,
+//                 title: answers.title,
+//                 description: answers.description, 
+//                 installation: answers.installation,
+//                 usages: answers.usage,
+//                 licenses: answers.licenses,
+//                 tests: answers.tests,
+//                 contribute: answers.contribute,
+//                 email: res.email,
+//                 pfp: res.avatar_url
+//             }
+//             const final = markdown(data)
+//             writeToFile("./output/README.md", final);
+//         })
+//         .cathc(err => {
+//             console.log('err ', err);
+//         });
+//     });
+// };
+
---
+++ utilities/markdown.js
@@ -0,0 +1,41 @@
+function markdown(data) {
+    return `
+
+    # ${data.title} ![GitHub package.json version]
+    ${data.description}
+
+    # Table of Contents
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    * [Credits](#credits)
+
+    # Installation
+    ${data.installation}
+
+    # Usage
+    ${data.licenses}
+
+    # Contributing
+    ${data.contribute}
+
+    # Tests
+    ${data.tests}
+
+    # Questions
+    ![Profile Avatar](${data.pfp})
+    For any question please e-mail at ${data.email}.
+
+    # Credits
+    Github: [${data.username}](${data.url})
+
+    Copyright ${data.name}. All rights reserved.
+
+    `;
+
+};
+
+module.exports = markdown;
